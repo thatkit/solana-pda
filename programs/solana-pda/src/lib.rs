3c05ddf083033d8ed225aad9264703b78a2fc85f
Nikita <emper137137@gmail.com>
2025-05-03 14:49:28 +1000

    add: create multiple PDAs with dynamic seeds

--- programs/solana-pda/src/lib.rs
+++ programs/solana-pda/src/lib.rs
@@ -1,36 +1,60 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EFMWmbXQqyFeDWtXvARgUZYF8WUHrsTMNY7fTJ27rYzq");
+declare_id!("Hz1rgv8CaJorxqGsMpuYLCcZJ9BNZEeghC7mNB4Wodsm");
 
 #[program]
 pub mod solana_pda {
     use super::*;
 
-    pub fn create(ctx: Context<CreatePDA>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, message: String) -> Result<()> {
+        msg!("Create Message: {}", message);
+        let account_data = &mut ctx.accounts.message_account;
+        account_data.user = ctx.accounts.user.key();
+        account_data.message = message;
+        account_data.bump = ctx.bumps.message_account;
         Ok(())
     }
 
-    pub fn get(ctx: Context<GetPDA>) -> Result<()> {
+    pub fn update(_ctx: Context<Update>) -> Result<()> {
         Ok(())
     }
 
-    pub fn update(ctx: Context<UpdatePDA>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn delete(ctx: Context<DeletePDA>) -> Result<()> {
+    pub fn delete(_ctx: Context<Delete>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreatePDA {}
+#[instruction(message: String)]
+pub struct Create<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [
+            b"message",
+            message.as_bytes(),
+            user.key().as_ref(),
+        ],
+        bump,
+        payer = user,
+        space = 8 + 32 + 4 + message.len() + 1,
+    )]
+    pub message_account: Account<'info, MessageAccount>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct GetPDA {}
+pub struct Update {}
 
 #[derive(Accounts)]
-pub struct UpdatePDA {}
+pub struct Delete {}
 
-#[derive(Accounts)]
-pub struct DeletePDA {}
+#[account]
+pub struct MessageAccount {
+    pub user: Pubkey,
+    pub message: String,
+    pub bump: u8,
+}
--- tests/solana-pda.ts
+++ tests/solana-pda.ts
@@ -1,29 +1,55 @@
+import { expect } from 'chai';
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
+import { PublicKey } from '@solana/web3.js';
 
 import { SolanaPda } from '../target/types/solana_pda';
 
+const generateUniqueSeedData = (): string => 'TEST' + String(Date.now());
+
 describe('solana-pda', () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.solanaPda as Program<SolanaPda>;
 
-  it('Should create', async () => {
-    const tx = await program.methods.create().rpc();
-    console.log('Tx signature:', tx);
+  const uniqueSeedData = generateUniqueSeedData();
+  const seeds = <Array<Buffer | Uint8Array>>[
+    Buffer.from('message'),
+    Buffer.from(uniqueSeedData),
+    provider.wallet.publicKey.toBuffer(),
+  ];
+
+  const [messagePda, messageBump] = PublicKey.findProgramAddressSync(
+    seeds,
+    program.programId
+  );
+
+  console.log('Expected PDA:', {
+    address: messagePda.toString(),
+    bump: messageBump,
   });
 
-  // it('Should get without fee', async () => {
+  it('Create Message Account', async () => {
+    const message = uniqueSeedData;
+    const transactionSignature = await program.methods
+      .create(message)
+      .accounts({
+        messageAccount: messagePda, // do we need this? (types are not generated correctly)
+      } as any)
+      .rpc({ commitment: 'confirmed' });
 
-  // });
+    const messageAccount = await program.account.messageAccount.fetch(
+      messagePda,
+      'confirmed',
+    );
 
-  it('Should update', async () => {
-    const tx = await program.methods.update().rpc();
-    console.log('Tx signature:', tx);
-  });
+    console.log(JSON.stringify(messageAccount, null, 2));
+    console.log(
+      'Transaction Signature:',
+      `https://explorer.solana.com/tx/${transactionSignature}?cluster=custom`,
+    );
 
-  it('Should delete', async () => {
-    const tx = await program.methods.delete().rpc();
-    console.log('Tx signature:', tx);
+    expect(messageAccount.bump).equal(messageBump);
   });
 });
